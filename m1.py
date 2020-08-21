09a88959b6835636c9875a2e03e0fc82ea2fff2a
Gr-Vit <exiledfromdarkparadise@gmail.com>
2020-08-22 02:04:14 +0500

    ‘izm'

--- m1.py
+++ m1.py
@@ -1,13 +1,14 @@
 import config
 import telebot
 
+
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, "Привет " 
+	bot.send_message(message.chat.id, "hi" 
     + message.from_user.first_name + ' ' +message.from_user.last_name
-    + ", я бот!")
+    + "lol")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
